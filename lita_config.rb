11d459ccc63c83f94592e2d2d5e344e77e9dd508
Joel Byler <jbyler@covermymeds.com>
2015-11-16 14:29:59 -0500

    Add redis to go url

--- lita_config.rb
+++ lita_config.rb
@@ -3,4 +3,6 @@ Lita.configure do |config|
   config.robot.name = "Lita"
   config.robot.adapter = :slack
   config.adapters.slack.token = ENV['LITA_SLACK_API_TOKEN']
+  config.redis[:url] = ENV["REDISTOGO_URL"]
+  config.http.port = ENV["PORT"]
 end
