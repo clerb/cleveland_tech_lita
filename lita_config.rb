429f65ff1f64b13ac5a188b809450ca381bfc234
Joel Byler <jbyler@covermymeds.com>
2015-11-16 14:58:18 -0500

    Fix deprication warning

--- lita_config.rb
+++ lita_config.rb
@@ -3,6 +3,7 @@ Lita.configure do |config|
   config.robot.name = "Lita"
   config.robot.adapter = :slack
   config.adapters.slack.token = ENV['LITA_SLACK_API_TOKEN']
-  config.redis.url = ENV["REDISTOGO_URL"]
+  config.redis[:url] = ENV["REDISTOGO_URL"]
   config.http.port = ENV["PORT"]
+  config.adapter.rooms = :all
 end
